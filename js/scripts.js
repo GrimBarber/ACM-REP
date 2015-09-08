3bb0dc0529cf81b2f6dbc7fbdb0bc896965ce37b
GrimBarber <achizhkoff@yahoo.com>
2015-09-08 16:42:06 +0300

    Last changes
    FINAL VERSION  08 09 105

--- js/scripts.js
+++ js/scripts.js
@@ -21,24 +21,67 @@ $(document).ready(function() {
 		slideBy: 3
 	});
 	
+	/*MASK*/
+	jQuery(function($) {
+		$("#date").mask("99/99/9999",{placeholder:"mm/dd/yyyy"});
+		$(".phone-mask").mask("(9 9 9) 9 9 9-9 9 9 9");
+		$("#tin").mask("99-9999999");
+		$("#ssn").mask("999-99-9999");
+	});
 	
-	$('.map-button').click(function() {
-		$('.hidden-popup').not($(this).find('.hidden-popup')).fadeOut(250)
-		$(this).parent().find('.hidden-popup').fadeToggle(250)
+	
+//	$('.map-button').click(function() {
+//		$('.hidden-popup').not($(this).find('.hidden-popup')).fadeOut(250)
+//		$(this).parent().find('.hidden-popup').fadeToggle(250)
+//	});
+//	$("body").on("click",function(e) {
+//		if (!$(e.target).hasClass("scheme-item") && !$(e.target).hasClass("rounded-background") && 			!$(e.target).parents().hasClass("scheme-item") && !$(e.target).parents().hasClass("rounded-background")) {
+//			$(".hidden-popup").fadeOut(250)
+//		}
+//		else if ($(e.target).hasClass("popup-close-button")) {
+//			$(".hidden-popup").fadeOut(250)
+//		}
+//	});
+
+	$(".hidden-popup .popup-close-button").on("click",function() {
+		$(this).parents(".hidden-popup").stop().fadeOut(250)
 	});
-	$("body").on("click",function(e) {
-		if (!$(e.target).hasClass("scheme-item") && !$(e.target).hasClass("rounded-background") && 			!$(e.target).parents().hasClass("scheme-item") && !$(e.target).parents().hasClass("rounded-background")) {
-			$(".hidden-popup").fadeOut(250)
-		}
-		else if ($(e.target).hasClass("popup-close-button")) {
-			$(".hidden-popup").fadeOut(250)
-		}
+	
+	var timerSecond;
+
+	$(".scheme-item").on("mouseover",function() {
+		$(".scheme-item").not($(this)).find(".hidden-popup").stop().fadeOut(250);
+		clearTimeout(timerSecond);
+		$(this).find(".hidden-popup").stop().fadeIn(250);
+	}).on("mouseout",function() {
+		var obj = $(this);
+		timerSecond = setTimeout(function() {
+			obj.find(".hidden-popup").stop().fadeOut(250);
+		},500)
 	});
+	
+	var timerThird;
+	
+	$(".small-images-holder").on("mouseover",function() {
+		$(".small-images-holder").not($(this)).find(".hidden-popup").stop().fadeOut(250);
+		clearTimeout(timerThird);
+		$(this).find(".hidden-popup").stop().fadeIn(250);
+	}).on("mouseout",function() {
+		var objSecond = $(this);
+		timerThird = setTimeout(function() {
+			objSecond.find(".hidden-popup").stop().fadeOut(250);
+		},500)
+	});
+	
+//	$(".order-item-button").click(function() {
+//		$("#item-id").html($(this).parents(".item-container").find(".model-name a").html());
+//	})
 	
 	$(".order-item-button").click(function() {
-		$("#item-id").html($(".model-name a").html());
+		$("#item-id").html($(this).data("name"));
+		$("#name").val($(this).data("name"));
+		$("#edit_url").val($(this).data("link"));
 	})
-	
 
 	// Формы
 	
@@ -77,15 +120,26 @@ $(document).ready(function() {
 		
 	});
 	
-	$(".services-button").click(function() {
-		$(".services-option").fadeToggle(250)
+	var timer;
+	
+	$(".menu-wrapper").on("mouseover",function() {
+		clearTimeout(timer);
+		$(".services-option").stop().fadeIn(250);
+	}).on("mouseout",function() {
+		timer = setTimeout(function() {
+			$(".services-option").stop().fadeOut(250);
+		},500)
 	});
-	$("body").on("click",function(e) {
-		if (!$(e.target).hasClass("services-option") && !$(e.target).hasClass("services-button") && !$(e.target).parents().hasClass("services-option") && !$(e.target).parents().hasClass("services-button")) {
-			$(".services-option").fadeOut(250)
-		}
-		console.log(e.target)
-	})
+	
+	// $(".services-button").on("mouseover",function() {
+		// $(".services-option").fadeIn(250)
+	// });
+	// $("body").on("mouseover",function(e) {
+		// if (!$(e.target).hasClass("services-descr") && !$(e.target).hasClass("services-option") && !$(e.target).hasClass("services-button") && !$(e.target).parents().hasClass("services-option") && !$(e.target).parents().hasClass("services-button")) {
+			// $(".services-option").fadeOut(250)
+		// }
+		// console.log(e.target)
+	// })
 	
 	$("body").on("click",".placeholder",function(e) {
 		if ($(this).parent().find("input").length) {
